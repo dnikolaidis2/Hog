6ca69fc305faa4bfcec5aadd7143ef3234faeff2
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2022-04-28 10:44:14 +0300

    Adds global shader data to renderer and shader

--- Vulkan-Core/src/VulkanCore/Renderer/Renderer.h
+++ Vulkan-Core/src/VulkanCore/Renderer/Renderer.h
@@ -9,13 +9,26 @@ namespace VulkanCore
 	class Renderer
 	{
 	public:
+		struct GlobalShaderData
+		{
+			glm::vec3 LightPosition;
+			int32_t LightMaterialIndex;
+		};
+
 		static void Initialize();
 		static void Deinitialize();
 
-		static void BeginScene(const EditorCamera& camera);
+		static void BeginScene(const EditorCamera& camera, GlobalShaderData& globalShaderData);
 		static void EndScene();
 
 		static void DrawObject(const Ref<RendererObject> object);
 		static void DrawObjects(const std::vector<Ref<RendererObject>>& objects);
+
+		struct RendererStats
+		{
+			uint64_t FrameCount = 0;
+		};
+
+		static RendererStats GetStats();
 	};
 }
