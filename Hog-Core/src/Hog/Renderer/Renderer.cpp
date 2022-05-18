f5e38e6e0777772f5e8922430941d3bd988923da
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2022-05-18 16:41:17 +0300

    Refactor Renderer to use per frame data

--- Hog-Core/src/Hog/Renderer/Renderer.cpp
+++ Hog-Core/src/Hog/Renderer/Renderer.cpp
@@ -16,24 +16,46 @@ namespace Hog
 	struct RendererStageData
 	{
 		RendererStage Info;
+		std::vector<VkDescriptorSet> DescriptorSets;
+	};
+
+	struct FrameData
+	{
 		VkCommandPool CommandPool;
 		VkCommandBuffer CommandBuffer;
 		VkFence Fence;
-		std::vector<VkDescriptorSet> DescriptorSets;
+		VkSemaphore PresentSemaphore;
+		VkSemaphore RenderSemaphore;
 	};
 
 	struct RendererData
 	{
 		RenderGraph Graph;
+		std::vector<FrameData> FrameData;
 		std::vector<RendererStageData> StageData;
 		DescriptorLayoutCache DescriptorLayoutCache;
 		DescriptorAllocator DescriptorAllocator;
+
+		uint32_t FrameIndex;
+		uint32_t MaxFrameCount;
 	};
 
 	static RendererData s_Data;
 
 	void Renderer::Initialize(RenderGraph renderGraph)
 	{
+		s_Data.MaxFrameCount = *CVarSystem::Get()->GetIntCVar("renderer.frameCount");
+		s_Data.FrameData.resize(s_Data.MaxFrameCount);
+
+		for (int i = 0; i < s_Data.FrameData.size(); ++i)
+		{
+			s_Data.FrameData[i].CommandPool = GraphicsContext::CreateCommandPool();
+			s_Data.FrameData[i].CommandBuffer = GraphicsContext::CreateCommandBuffer(s_Data.FrameData[0].CommandPool);
+			s_Data.FrameData[i].Fence = GraphicsContext::CreateFence(true);
+			s_Data.FrameData[i].PresentSemaphore = GraphicsContext::CreateVkSemaphore();
+			s_Data.FrameData[i].RenderSemaphore = GraphicsContext::CreateVkSemaphore();
+		}
+
 		s_Data.Graph = renderGraph;
 		s_Data.DescriptorAllocator.Init(GraphicsContext::GetDevice());
 		s_Data.DescriptorLayoutCache.Init(GraphicsContext::GetDevice());
@@ -41,9 +63,6 @@ namespace Hog
 		auto stages = s_Data.Graph.GetFinalStages();
 		s_Data.StageData.resize(stages.size());
 		s_Data.StageData[0].Info = stages[0]->StageInfo;
-		s_Data.StageData[0].CommandPool = GraphicsContext::CreateCommandPool();
-		s_Data.StageData[0].CommandBuffer = GraphicsContext::CreateCommandBuffer(s_Data.StageData[0].CommandPool);
-		s_Data.StageData[0].Fence = GraphicsContext::CreateFence(false);
 
 		if (s_Data.StageData[0].Info.Resources.ContainsType(ResourceType::Constant))
 		{
@@ -106,64 +125,85 @@ namespace Hog
 	{
 		HG_PROFILE_FUNCTION();
 
+		auto& currentFrame = s_Data.FrameData[s_Data.FrameIndex];
+
+		CheckVkResult(vkWaitForFences(GraphicsContext::GetDevice(), 1, &currentFrame.Fence, VK_TRUE, UINT64_MAX));
+		vkResetFences(GraphicsContext::GetDevice(), 1, &currentFrame.Fence);
+
+		vkAcquireNextImageKHR(GraphicsContext::GetDevice(), GraphicsContext::GetSwapchain(), UINT64_MAX, currentFrame.PresentSemaphore, VK_NULL_HANDLE, &s_Data.FrameIndex);
+
+		// begin command buffer
+		VkCommandBufferBeginInfo beginInfo{};
+		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+		CheckVkResult(vkBeginCommandBuffer(currentFrame.CommandBuffer, &beginInfo));
+
 		for (auto& stage : s_Data.StageData)
 		{
-			// begin command buffer
-			VkCommandBufferBeginInfo beginInfo{};
-			beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-
-			CheckVkResult(vkBeginCommandBuffer(stage.CommandBuffer, &beginInfo));
-
 			for (auto& resource : stage.Info.Resources)
 			{
 				if (resource.Type == ResourceType::Storage)
 				{
-					resource.Buffer->LockAfterWrite(stage.CommandBuffer, VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT);
+					resource.Buffer->LockAfterWrite(currentFrame.CommandBuffer, VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT);
 				}
 			}
 
-			stage.Info.Shader->Bind(stage.CommandBuffer);
-			vkCmdBindDescriptorSets(stage.CommandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, stage.Info.Shader->GetPipelineLayout(),
+			stage.Info.Shader->Bind(currentFrame.CommandBuffer);
+			vkCmdBindDescriptorSets(currentFrame.CommandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, stage.Info.Shader->GetPipelineLayout(),
 				0, (uint32_t)stage.DescriptorSets.size(), stage.DescriptorSets.data(), 0, 0);
 
-			vkCmdDispatch(stage.CommandBuffer, 32, 1, 1);
+			vkCmdDispatch(currentFrame.CommandBuffer, 32, 1, 1);
 
 			for (auto& resource : stage.Info.Resources)
 			{
 				if (resource.Type == ResourceType::Storage)
 				{
-					resource.Buffer->LockBeforeRead(stage.CommandBuffer, VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT);
+					resource.Buffer->LockBeforeRead(currentFrame.CommandBuffer, VK_PIPELINE_STAGE_2_COMPUTE_SHADER_BIT);
 				}
 			}
-
-			// Submit compute work
-			vkResetFences(GraphicsContext::GetDevice(), 1, &stage.Fence);
-
-			// end command buffer
-			CheckVkResult(vkEndCommandBuffer(stage.CommandBuffer));
-
-			const VkCommandBufferSubmitInfo commandBufferSubmitInfo = {
-				.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO,
-				.commandBuffer = stage.CommandBuffer,
-			};
-
-			const VkSubmitInfo2 submitInfo = {
-				.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO_2,
-				.commandBufferInfoCount = 1,
-				.pCommandBufferInfos = &commandBufferSubmitInfo,
-			};
-
-			CheckVkResult(vkQueueSubmit2(GraphicsContext::GetComputeQueue(), 1, &submitInfo, stage.Fence));
-			CheckVkResult(vkWaitForFences(GraphicsContext::GetDevice(), 1, &stage.Fence, VK_TRUE, UINT64_MAX));
 		}
+
+		// end command buffer
+		CheckVkResult(vkEndCommandBuffer(currentFrame.CommandBuffer));
+
+		const VkCommandBufferSubmitInfo commandBufferSubmitInfo = {
+			.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO,
+			.commandBuffer = currentFrame.CommandBuffer,
+		};
+
+		const VkSemaphoreSubmitInfo waitSemaphoreInfo = {
+			.sType = VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO,
+			.semaphore = currentFrame.PresentSemaphore,
+		};
+
+		const VkSemaphoreSubmitInfo signalSemaphoreInfo = {
+			.sType = VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO,
+			.semaphore = currentFrame.RenderSemaphore,
+		};
+
+		const VkSubmitInfo2 submitInfo = {
+			.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO_2,
+			.waitSemaphoreInfoCount = 1,
+			.pWaitSemaphoreInfos = &waitSemaphoreInfo,
+			.commandBufferInfoCount = 1,
+			.pCommandBufferInfos = &commandBufferSubmitInfo,
+			.signalSemaphoreInfoCount = 1,
+			.pSignalSemaphoreInfos = &signalSemaphoreInfo,
+		};
+
+		CheckVkResult(vkQueueSubmit2(GraphicsContext::GetGraphicsQueue(), 1, &submitInfo, currentFrame.Fence));
+
+		s_Data.FrameIndex = (s_Data.FrameIndex + 1) % s_Data.MaxFrameCount;
 	}
 
 	void Renderer::Deinitialize()
 	{
-		for (const auto& stage : s_Data.StageData)
+		for (const auto& frame : s_Data.FrameData)
 		{
-			vkDestroyFence(GraphicsContext::GetDevice(), stage.Fence, nullptr);
-			vkDestroyCommandPool(GraphicsContext::GetDevice(), stage.CommandPool, nullptr);
+			vkDestroyFence(GraphicsContext::GetDevice(), frame.Fence, nullptr);
+			vkDestroyCommandPool(GraphicsContext::GetDevice(), frame.CommandPool, nullptr);
+			vkDestroySemaphore(GraphicsContext::GetDevice(), frame.PresentSemaphore, nullptr);
+			vkDestroySemaphore(GraphicsContext::GetDevice(), frame.RenderSemaphore, nullptr);
 		}
 
 		s_Data.StageData.clear();
