ef60a91960b48c4b82554faf59fcc2773393cd4c
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2022-05-18 12:08:25 +0300

    Cleanup Renderer.h file

--- Hog-Core/src/Hog/Renderer/Renderer.h
+++ Hog-Core/src/Hog/Renderer/Renderer.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include "Hog/Renderer/RendererObject.h"
-#include "Hog/Core/Base.h"
-#include "Hog/Renderer/EditorCamera.h"
 #include "Hog/Renderer/RenderGraph.h"
 
 namespace Hog
@@ -10,12 +7,6 @@ namespace Hog
 	class Renderer
 	{
 	public:
-		struct GlobalShaderData
-		{
-			glm::vec3 LightPosition;
-			int32_t LightMaterialIndex;
-		};
-
 		static void Initialize(RenderGraph renderGraph);
 		static void Deinitialize();
 
