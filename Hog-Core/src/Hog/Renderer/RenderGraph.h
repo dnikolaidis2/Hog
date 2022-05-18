2a8afecd4449075629f02784ebb549d0c4e27a1c
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2022-05-18 12:05:55 +0300

    Small formatting fix

--- Hog-Core/src/Hog/Renderer/RenderGraph.h
+++ Hog-Core/src/Hog/Renderer/RenderGraph.h
@@ -25,17 +25,17 @@ namespace Hog
 		{
 			switch (Type)
 			{
-			case DataType::Float:   return 1;
-			case DataType::Float2:  return 2;
-			case DataType::Float3:  return 3;
-			case DataType::Float4:  return 4;
-			case DataType::Mat3:    return 3; // 3* float3
-			case DataType::Mat4:    return 4; // 4* float4
-			case DataType::Int:     return 1;
-			case DataType::Int2:    return 2;
-			case DataType::Int3:    return 3;
-			case DataType::Int4:    return 4;
-			case DataType::Bool:    return 1;
+				case DataType::Float:   return 1;
+				case DataType::Float2:  return 2;
+				case DataType::Float3:  return 3;
+				case DataType::Float4:  return 4;
+				case DataType::Mat3:    return 3; // 3* float3
+				case DataType::Mat4:    return 4; // 4* float4
+				case DataType::Int:     return 1;
+				case DataType::Int2:    return 2;
+				case DataType::Int3:    return 3;
+				case DataType::Int4:    return 4;
+				case DataType::Bool:    return 1;
 			}
 
 			HG_CORE_ASSERT(false, "Unknown DataType!");
