b72a8bf9e47cafabbd013eef44a6ab3efdc40402
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2022-05-23 11:09:43 +0300

    Add debug log for VMA when in debug mode

--- Hog-Core/vendor/vma/vk_mem_alloc.cpp
+++ Hog-Core/vendor/vma/vk_mem_alloc.cpp
@@ -1,4 +1,12 @@
 #include "hgpch.h"
 
 #define VMA_IMPLEMENTATION
+
+#if HG_DEBUG
+	#define VMA_DEBUG_LOG(format, ...) do { \
+	       printf(format, __VA_ARGS__); \
+	       printf("\n"); \
+	   } while(false)
+#endif
+
 #include "vk_mem_alloc.h"
